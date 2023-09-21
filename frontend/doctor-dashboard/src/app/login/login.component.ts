90b1e6b142a199c83f1c093df3a53887297060a3
LikithaParise <Likhitha.sunny@gmail.com>
2023-09-21 05:44:35 +0530

    DoctorApp

--- frontend/doctor-dashboard/src/app/login/login.component.ts
+++ frontend/doctor-dashboard/src/app/login/login.component.ts
@@ -4,57 +4,52 @@ import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
-  username: string |any;
-  password: string |any;
-  usertype:string |any;
-
-
+  username: string | any;
+  password: string | any;
+  usertype: string | any;
 
   private _isLoggedIn: boolean = false; // private backing field for isLoggedIn
 
-
-  constructor(private authService: AuthService,private router: Router,private http: HttpClient) {
-
-  }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   login() {
-
     console.log(this.username);
     console.log(this.usertype);
     console.log(this.password);
-    this.authService.login(this.username, this.password, this.usertype).subscribe(
-      (response: any) => {
-        // set token received from server
-        this.authService.setToken(response.token);
-        localStorage.setItem('usertype', this.usertype); // store usertype in localStorage
-        localStorage.setItem('password', this.password); // store usertype in localStorage
-        localStorage.setItem('username', this.username); // store usertype in localStorage
-        this._isLoggedIn = true; // modify the private backing field
+    this.authService
+      .login(this.username, this.password, this.usertype)
+      .subscribe(
+        (response: any) => {
+          // set token received from server
+          this.authService.setToken(response.token);
+          localStorage.setItem('usertype', this.usertype); // store usertype in localStorage
+          localStorage.setItem('password', this.password); // store usertype in localStorage
+          localStorage.setItem('username', this.username); // store usertype in localStorage
+          this._isLoggedIn = true; // modify the private backing field
 
-        // window.location.reload();
-        alert(`Welcome to MyDoctorApp ${this.username}!`);
-        location.href = "/";
-        // this.router.navigate(['/']); // navigate to the jobs page
+          // window.location.reload();
+          alert(`Welcome to Doctor Booking App ${this.username}!`);
 
-
-
-      },
-      (error) => {
-        this.router.navigate(['/login']);
-        console.log(error);
-        alert('Invalid Username or Password or Usertype Failed! ');
-      }
-    );
+          location.href = '/';
+          // this.router.navigate(['/']); // navigate to the jobs page
+        },
+        (error) => {
+          this.router.navigate(['/login']);
+          console.log(error);
+          alert('Invalid Username or Password or Usertype Failed! ');
+        }
+      );
   }
-
 
   get isLoggedIn() {
     return this._isLoggedIn; // return the private backing field
